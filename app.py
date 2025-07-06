84e3121e38621afa77ecf8b95bd2eaa25b1b5c84
zoricheva <zoricheva@yahoo.com>
2025-07-06 16:51:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ pip install gunicorn
 from flask import Flask, render_template, request
 from process import get_prediction
 
+pip freeze > requirements.txt
+
 app = Flask(__name__)
 
 @app.route('/', methods = ["get","post"])
