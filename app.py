69f9898507ce111be7c679d36541ac22e0fffa80
zoricheva <zoricheva@yahoo.com>
2025-07-06 23:21:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ def hello():
   message = ''
   if request.method == 'POST':
     area = request.form.get('area')
-    area = float(area)
+    try:
+      area = float(area)
+    except Exception as e:
+      print(e)
+      message += "Некорректный ввод. Установленно значение по умолчанию:0"
     cost = get_prediction(area)
     message = f"Стоимость квартиры площадью {area} равна {cost} рублей"
   return render_template('index.html', message=message)
