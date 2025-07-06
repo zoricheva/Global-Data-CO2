9237015243a1efb72b853b6c21c7c2c50c72f603
zoricheva <zoricheva@yahoo.com>
2025-07-06 20:04:12 +0300

    Update process.py

--- process.py
+++ process.py
@@ -1,4 +1,4 @@
-def get_prediction (area):
+def get_prediction(area):
   #TODO: Добавить загрузку модели из файла
   # TODO: Добавить получение предсказаний загруженной модели
   cost = 300_000 * area + 120_000
