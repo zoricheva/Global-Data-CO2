6e672222f58943634b38aacf10f48a2fb6592e90
zoricheva <zoricheva@yahoo.com>
2025-07-11 11:39:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,20 +10,18 @@ app = flask.Flask(__name__, template_folder = 'templates')
 @app.route('/', methods = ['POST','GET'])
 
 @app.route('/index', methods = ['POST','GET'])
-def hello():
+def main():
+    if flask.request.method == 'GET':
+        return render_template('index.html')
+    
+    if flask.request.method == 'POST':
+        with open('model.pkl', 'rb') as f:
+            loaded_model = pickle.load(f)
 
-#функция
-if flask.request.method == 'GET':
-  return render_template('index.html')
-  
-if flask.request.method == 'POST':
-  with open('model.pkl', 'rb') as f:
-    loaded_model = pickle.load(f)
+        En = float(flask.request.form ['energy'])
+        y_pred = loaded_model.predict([(En)])
 
-    En = float(flask.request.form ['energy'])
-    y_pred = loaded_model.predict([(En)])
-
-    return render_template('index.html', result = y_pred)
+        return render_template('index.html', result = y_pred)
 
  
 
