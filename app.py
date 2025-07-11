7c3595b5cc1c0998e3f0db4c5009b31c9d48485a
zoricheva <zoricheva@yahoo.com>
2025-07-11 12:33:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,17 +11,20 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['POST','GET'])
 def hello():
-  message = ''
+  result = ''
   if request.method == 'POST':
+    try:
+      En = float(flask.request.form ['energy'])
+         
       with open('model.pkl', 'rb') as f:
-            loaded_model = pickle.load(f)
-        try:
-          En = float(flask.request.form ['energy'])
-        except Exception as e:
-          print(e)
-          result += "Некорректный ввод. Установленно значение по умолчанию:0"
-              y_pred = loaded_model.predict([(En)])
-          return render_template('index.html', result = y_pred)
+        loaded_model = pickle.load(f)
+    except Exception as e:
+        print(e)
+        result += f"Некорректный ввод. Установленно значение по умолчанию:0"
+        En = 0.0
+    y_pred = loaded_model.predict([(En)])
+    result += f"Предсказание:{y_pred}"
+   return render_template('index.html', result=result)
 
     
 
