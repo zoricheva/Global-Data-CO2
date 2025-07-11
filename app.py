cf0643ec6087e6f2ce6d32934cd5c5d89702c4e6
zoricheva <zoricheva@yahoo.com>
2025-07-11 09:33:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, request
 from process import get_prediction
+import pickle
+import sklearn
+from sklearn.linear_model import LinearRegression
 
-app = Flask(__name__)
 
-@app.route('/', methods = ['post','get'])
+app = flask.Flask(__name__, template_folder = 'template')
+
+@app.route('/', methods = ['POST','GET'])
+
+@app.route('/index.html', methods = ['POST','GET'])
+def main:
+
+
 def hello():
   message = ''
   if request.method == 'POST':
@@ -15,6 +24,7 @@ def hello():
       message += "Некорректный ввод. Установленно значение по умолчанию:0"
     cost = get_prediction(area)
     message = f"Стоимость квартиры площадью {area} равна {cost} рублей"
+    
   return render_template('index.html', message=message)
 
   
