71bbd83bf218249010625e69bdcfb51d6ce1fd3b
zoricheva <zoricheva@yahoo.com>
2025-07-12 06:59:22 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
+import numpy as np
 from flask import Flask, render_template, request
 from process import get_prediction
+import pickle
+import sklearn 
+from sklearn.linear_model import LinearRegression
+
+
 
 app = Flask(__name__)
 
-@app.route('/', methods = ["get","post"])
+@app.route('/', methods = ['POST','GET'])
 def hello():
-  message = ""
-  if request.method == "POST":
-    area = requst.form.get("area")
-    #TODO: Добавить проверку ввода
-  area = float(area)
-  cost = get_prediction(area)
-  message = f"Стоимость квартиры площадью {area} равна {cost} рублей"
-  return render_template("index.html", message = message)
+  message = ''
+  if request.method == 'POST':
+    try:
+      En = flask.request.form ['energy']
+         
+      with open('model.pkl', 'rb') as f:
+        loaded_model = pickle.load(f)
+    except Exception as e:
+        print(e)
+        message += "Некорректный ввод. Установленно значение по умолчанию:0"
+        En = 0.0
+    y_pred = loaded_model.predict([(En)])
+    message = f"Предсказание:{y_pred}"
+  return render_template('index.html', result = message)
+
+
+
+
+    
+
+ 
+
+  
