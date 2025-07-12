175db4faedb03f01f08039e7ba9a0e2bd1c6f68b
zoricheva <zoricheva@yahoo.com>
2025-07-12 07:26:08 +0300

    Update process.py

--- process.py
+++ process.py
@@ -1,5 +1,7 @@
-def get_prediction(area):
-  #TODO: Добавить загрузку модели из файла
-  # TODO: Добавить получение предсказаний загруженной модели
-  cost = 300_000 * area + 120_000
-  return cost
+
+
+def loaded_model(energy):
+  with open('model.pkl', 'rb') as f:
+            loaded_model = pickle.load(f)
+
+  return y_pred
