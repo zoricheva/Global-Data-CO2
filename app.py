2747933c996e2d247c9a59f2d5adb301a79b00ef
zoricheva <zoricheva@yahoo.com>
2025-08-04 17:19:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request
-from process import get_prediction
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
-
-  return render_template("index.html")
+    return render_template("index.html")
