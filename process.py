c279be5fe91429ea5541d94bf5d567754e2d80c4
zoricheva <zoricheva@yahoo.com>
2025-08-04 19:34:00 +0300

    Update process.py

--- process.py
+++ process.py
@@ -1,8 +1,20 @@
+import pickle
+
 def get_prediction(energy):
+    
     # TODO: Добавить загрузку модели из файла
     # Примечание: если использовалась предобработка, то нужно тоже ее выполнить в коде приложения
     # TODO: Добавить получение предсказаний загруженной модели
-    co2 = 400 * energy + 345
+    #co2 = 400 * energy + 345
+    
+    with open('model.pkl', 'rb') as f:
+        loaded_model = pickle.load(f)
+        
+    co2 = loaded_model.predict([(energy)])
+
+
+    
     return co2
+
 
 
