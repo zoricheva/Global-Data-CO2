6fadeae1bb505fe971e6c4d5d8b7106b6f523f68
zoricheva <zoricheva@yahoo.com>
2025-08-04 13:28:35 +0300

    форма для ввода app.py

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['POST','GET'])
 def hello():
+  if request.method == 'POST':
+    energy = request.form.get("energy")
+    print(energy)
 
+  
   return render_template('index.html', result = message)
 
 
