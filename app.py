0e01f0f6f5f5b6776a0b6d3647c81e785de6e7e9
zoricheva <zoricheva@yahoo.com>
2025-08-04 19:17:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ def hello():
     energy = request.form.get("energy")
     try:
         energy = float(energy)
-        except Exception as e:
-            print(e)
-            message += "Некорректный ввод. Установлено значение по умолчанию: 0 "
-            energy = 0.0
-    
+    except Exception as e:
+      print(e)
+      message += "Некорректный ввод. Установлено значение по умолчанию: 0 "
+      energy = 0.0
+
     co2 = get_prediction(energy)
     message = f"При потребляемой энергии в размере {energy} количество выбросов будет равно {co2} "
     
