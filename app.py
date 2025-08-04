afe7c402dfa6c5ba67c7950940bf572dcb01ad8f
zoricheva <zoricheva@yahoo.com>
2025-08-04 18:19:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request
-
+from process import get_prediction
 
 
 app = Flask(__name__)
 
 @app.route('/', methods=["get", "post"])
 def hello():
-
+  
   if request.method == "POST":
     energy = requst.form.get("energy")
-    print(energy)
+    
+  energy = float(energy)
+  co = get_prediction(energy)
+  print(energy)
     
   return render_template("index.html")
