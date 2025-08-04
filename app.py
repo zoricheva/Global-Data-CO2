dcf419c3ea58fa38690061f4cafbb65504b3d9e5
zoricheva <zoricheva@yahoo.com>
2025-08-04 16:55:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,7 @@ from process import get_prediction
 
 app = Flask(__name__)
 
-@app.route('/', methods = ["get","post"])
+@app.route('/hello')
 def hello():
-  message = ""
-  if request.method == "POST":
-    area = requst.form.get("area")
-    #TODO: Добавить проверку ввода
-  area = float(area)
-  cost = get_prediction(area)
-  message = f"Стоимость квартиры площадью {area} равна {cost} рублей"
-  return render_template("index.html", message = message)
+
+  return "Hello!"
