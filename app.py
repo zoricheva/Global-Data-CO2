1ea8e997dabf4e826b0ec8cd82fa29339b5a4aa3
zoricheva <zoricheva@yahoo.com>
2025-08-04 18:26:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=["get", "post"])
 def hello():
-  
+  message = ""
   if request.method == "POST":
     energy = requst.form.get("energy")
     energy = float(energy)
     co = get_prediction(energy)
+    message = f"Стоимость квартиры площадью {energy} равна {co} рублей"
     print(co)
   
   
