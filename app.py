1b0057a8969c6351b29c2e2d9e39be27b935aefc
zoricheva <zoricheva@yahoo.com>
2025-08-04 13:43:21 +0300

    сообщение результата app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request
+from process import get_prediction
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['POST','GET'])
 def hello():
+  message = ''
   if request.method == 'POST':
     energy = request.form.get("energy")
-    print(energy)
+    energy = float(energy)
+    co2 = get_prediction(energy)
+    print(co2)
+    message = f"Предсказание:{y_pred}"
 
   
   return render_template('index.html', result = message)
