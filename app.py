5b551b92f52004dae5eb20eedd0545a4d8c552f4
zoricheva <zoricheva@yahoo.com>
2025-08-04 10:12:45 +0300

    Разработано базовое flask приложение

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['POST','GET'])
+def hello():
+
+  return render_template('index.html', result = message)
+
+
+if __name__
+app.run
+
 
