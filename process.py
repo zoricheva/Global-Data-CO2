475272e43d8c71475415b151685870dc55bf5ae9
zoricheva <zoricheva@yahoo.com>
2025-08-05 18:26:26 +0300

    Update process.py

--- process.py
+++ process.py
@@ -5,19 +5,20 @@ def get_prediction(energy):
     # TODO: Добавить загрузку модели из файла
     # Примечание: если использовалась предобработка, то нужно тоже ее выполнить в коде приложения
     # TODO: Добавить получение предсказаний загруженной модели
-    #co2 = 400 * energy + 345
+    co2 = 400 * energy + 345
     
    # with open('model.pkl', 'rb') as f:
        # loaded_model = pickle.load(f)
     
-    linear_model = pickle.load(open('model2.pkl','rb'))
-    co2 = linear_model.predict([[energy]])
+   # linear_model = pickle.load(open('model2.pkl','rb'))
+    #co2 = linear_model.predict([[energy]])
         
     #co2 = loaded_model.predict([(energy)])
 
 
     
     return co2
+
 
 
 
