19e9005fd9b21ea9f601e01027c1de59cdf393db
zoricheva <zoricheva@yahoo.com>
2025-08-06 13:53:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,8 @@ def hello():
       message += "Некорректный ввод. Установлено значение по умолчанию: 0 "
       energy = 0.0
 
-    def get_prediction(energy):
-    co2 = 400 * energy + 345
-    return co2
+    co2 =  get_prediction(energy)
+ 
     
     message = f"При потребляемой энергии в размере {energy} количество выбросов будет равно {co2} "
     
