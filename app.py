c1ad2f1210086de0f161a8032559c880ee487223
zoricheva <zoricheva@yahoo.com>
2025-08-06 21:50:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,25 +9,36 @@ app = Flask(__name__)
 
 @app.route('/', methods = ["get","post"])
 
-def index():
-  
-  if request.method == "GET": 
-    return render_template('index.html')
-    
+def hello():
+  message = ""
   if request.method == "POST":
+    energy = request.form.get("energy")
+    try:
+        energy = float(energy)
+
+    co2 = get_prediction(energy)
+    message = f"При потребляемой энергии в размере {energy} количество выбросов будет равно {co2} "
+
+  return render_template("index.html", message = message)
+
+
+  
+ # if request.method == "GET": 
+  #  return render_template('index.html')
+    
+  #if request.method == "POST":
     #en = request.form.get('energy')
     
    # with open('model_d.pkl','rb') as f:
     #  loaded_model = pickle.load(f)
 
-   
    # co2 = loaded_model.predict(en)
 
     
-    energy = request.form.get("energy")
-    co2 = get_prediction(energy)
+    #energy = request.form.get("energy")
+    #co2 = get_prediction(energy)
 
     
-    return render_template('index.html', result = co2)
+   # return render_template('index.html', result = co2)
 
     
