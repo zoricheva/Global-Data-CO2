3b78f54b772642dd7017365941fb5d874e825e54
zoricheva <zoricheva@yahoo.com>
2025-08-06 22:37:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def hello():
 
     with open('model_ex.pkl','rb') as f:
       loaded_model = pickle.load(f)
-    co2 = loaded_model.get_predict(energy)
+      co2 = loaded_model.get_prediction(energy)
     
     message = f"При потребляемой энергии в размере {energy} количество выбросов будет равно {co2} "
     
