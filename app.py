63a2ac7206d1cbfca61fc610662af2a3e94a2908
zoricheva <zoricheva@yahoo.com>
2025-08-06 16:54:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,16 @@ app = Flask(__name__)
 
 @app.route('/', methods = ["get","post"])
 
-def index():
+def hello():
   
+  if request.method == "GET": 
+  return render_template('index.html')
     
   if request.method == "POST":
     with open('model_d.pkl', 'rb') as f:
       loaded_model = pickle.load(f)
 
-    en = request.form.get('energy')
+    en = float(request.form.get('energy'))
     
     co2 = loaded_model.predict([en])
 
