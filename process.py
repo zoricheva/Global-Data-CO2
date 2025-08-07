090129399bb7c8691861dc0e46e2a219f60917f0
zoricheva <zoricheva@yahoo.com>
2025-08-07 18:19:31 +0300

    Update process.py

--- process.py
+++ process.py
@@ -7,7 +7,7 @@ def get_prediction(energy):
    linear_model = pickle.load(open('model_xxxx.pkl','rb'))
 
    minmax_scaler = MinMaxScaler()
-   X_test = minmax_scaler.transform(X_test['Electricity from fossil fuels (TWh)'])
+   X_test = minmax_scaler.transform(X_test)
 
    co2 = linear_model.predict([[X_test]])
 
@@ -24,6 +24,7 @@ def get_prediction(energy):
  
         
     #co2 = loaded_model.predict([(energy)])
+
 
 
 
