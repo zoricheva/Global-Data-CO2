e8450a1d190352c85fb198fe1c5a5470393e8c12
zoricheva <zoricheva@yahoo.com>
2025-08-09 10:56:01 +0300

    Update process.py

--- process.py
+++ process.py
@@ -10,7 +10,7 @@ def get_prediction(energy):
    minmax_scaler_X = MinMaxScaler()
    X = minmax_scaler_X.fit_transform([X])
    #X_test = minmax_scaler.transform(np.array(X_test))
-   X_test = minmax_scaler_X.transform([X])
+   X_test = minmax_scaler_X.transform(X)
    
    co2 = linear_model.predict(X_test)
 
@@ -27,6 +27,7 @@ def get_prediction(energy):
  
         
     #co2 = loaded_model.predict([(energy)])
+
 
 
 
