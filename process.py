6fae5be06c77a17da597bc8ccafd4acb772fe975
zoricheva <zoricheva@yahoo.com>
2025-08-09 14:50:27 +0300

    Update process.py

--- process.py
+++ process.py
@@ -8,12 +8,13 @@ def get_prediction(energy):
    X_test = [energy]
    
    linear_model = pickle.load(open('model41.pkl','rb'))
+   minmax_scaler_X = pickle.load(open('model41.pkl','rb'))
    
    X_test = pd.DataFrame(X_test,columns=['Electricity from fossil fuels (TWh)','Electricity from nuclear (TWh)','Electricity from renewables (TWh)'])
 
    #minmax_scaler_X = MinMaxScaler()
    #minmax_scaler_X.fit([X])
-   X_test = linear_model.transform(X_test)
+   X_test = minmax_scaler_X.transform(X_test)
    
    co2 = linear_model.predict(X_test)
 
