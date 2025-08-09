e3e52ccb600a8bc1792d1ac3879713ac6106ac69
zoricheva <zoricheva@yahoo.com>
2025-08-09 11:31:56 +0300

    Update process.py

--- process.py
+++ process.py
@@ -9,24 +9,17 @@ def get_prediction(energy):
 
    minmax_scaler_X = MinMaxScaler()
    minmax_scaler_X.fit([X])
-   #X_test = minmax_scaler.transform(np.array(X_test))
    X_test = minmax_scaler_X.transform([X])
    
    co2 = linear_model.predict(X_test)
 
    return co2
 
+
+#X_test = minmax_scaler.transform(np.array(X_test))
+
    
-    # TODO: Добавить загрузку модели из файла
-    # Примечание: если использовалась предобработка, то нужно тоже ее выполнить в коде приложения
-    # TODO: Добавить получение предсказаний загруженной модели
-    #co2 = 400 * energy + 345
-    
-   # with open('model.pkl', 'rb') as f:
-       # loaded_model = pickle.load(f)
- 
-        
-    #co2 = loaded_model.predict([(energy)])
+
 
 
 
