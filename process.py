fabc02b42cf3bb3b931e736f714c3f79154ae722
zoricheva <zoricheva@yahoo.com>
2025-08-09 16:30:28 +0300

    Update process.py

--- process.py
+++ process.py
@@ -6,20 +6,22 @@ from sklearn.preprocessing import MinMaxScaler
 
 def get_prediction(energy):
    X_test = [energy]
-   
-   linear_model = pickle.load(open('model41.pkl','rb'))
-   minmax_scaler_X = pickle.load(open('model41.pkl','rb'))
+
+   pipeline = pickle.load(open('pipline.pkl', 'rb'))
+   #linear_model = pickle.load(open('model41.pkl','rb'))
+   #minmax_scaler_X = pickle.load(open('model41.pkl','rb'))
    
    X_test = pd.DataFrame(X_test,columns=['Electricity from fossil fuels (TWh)','Electricity from nuclear (TWh)','Electricity from renewables (TWh)'])
 
    #minmax_scaler_X = MinMaxScaler()
    #minmax_scaler_X.fit([X])
-   X_test = minmax_scaler_X.transform(X_test)
-
- 
+   #X_test = minmax_scaler_X.transform(X_test)
 
    
-   co2 = linear_model.predict(X_test)
+   co2 = pipeline.predict(X_test)
+
+   
+   #co2 = linear_model.predict(X_test)
 
    #co2 = minmax_scaler_y.inverse_transform(co2)
 
@@ -29,6 +31,7 @@ def get_prediction(energy):
 #X_test = minmax_scaler.transform(np.array(X_test))
 
    
+
 
 
 
