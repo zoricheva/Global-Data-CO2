4941424abcceade07a4d0e28b9c41ea9db93df75
zoricheva <zoricheva@yahoo.com>
2025-08-13 21:37:42 +0300

    загрузка из файлов process.py

--- process.py
+++ process.py
@@ -4,6 +4,10 @@ from sklearn.preprocessing import MinMaxScaler
 
 def get_prediction(energy):
    X_test = [energy]
+
+   linear_model = pickle.load(open('model.pkl','rb'))
+   minmax_scaler_X = pickle.load(open('scaler.pkl','rb'))
+   minmax_scaler_y = pickle.load(open('scaler_y.pkl','rb'))
   
   return co2
 
