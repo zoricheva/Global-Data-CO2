75279178f4f273be4da92d8dcee103766322c182
zoricheva <zoricheva@yahoo.com>
2025-08-13 21:26:48 +0300

    добавила методы app.py

--- app.py
+++ app.py
@@ -9,6 +9,14 @@ app = Flask(__name__)
 @app.route('/', methods = ["get","post"])
 
 def hello():
+  if request.method == "POST":
+    energy1 = request.form.get("energy1")
+    energy2 = request.form.get("energy2")
+    energy3 = request.form.get("energy3")
+    try:
+        energy1 = float(energy1)
+        energy2 = float(energy2)
+        energy3 = float(energy3)
 
 
 
