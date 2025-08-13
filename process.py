7fd7f50be6e95d675792343103f75d00017ffd52
zoricheva <zoricheva@yahoo.com>
2025-08-13 21:38:48 +0300

    нормализация process.py

--- process.py
+++ process.py
@@ -8,6 +8,12 @@ def get_prediction(energy):
    linear_model = pickle.load(open('model.pkl','rb'))
    minmax_scaler_X = pickle.load(open('scaler.pkl','rb'))
    minmax_scaler_y = pickle.load(open('scaler_y.pkl','rb'))
+
+   X_test = pd.DataFrame(X_test,columns=['Electricity from fossil fuels (TWh)','Electricity from nuclear (TWh)','Electricity from renewables (TWh)'])
+
+   X_test = minmax_scaler_X.transform(X_test)
+   
+   co2 = minmax_scaler_y.inverse_transform(co2)
   
   return co2
 
